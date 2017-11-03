05e21b2b84ada36c15cca13bea674dd637ec453f
Arthur Brophy <artbrophy@gmail.com>
2017-11-02 21:28:27 -0400

    completed day 3

---
+++ practice/03.exs
@@ -0,0 +1,59 @@
+defmodule CowInterrogator do
+
+  @doc """
+  Gets name from standard IO
+  """
+  def get_name do
+    IO.gets("What's your name? ")
+    |> String.trim
+  end
+
+  @doc """
+  asks if you love cows
+  """
+  def get_cow_lover do
+    IO.getn("Do you like cows? [y|n]", 1)
+  end
+
+  @doc """
+  runs the full interrogation procedure
+  """
+  def interrogate do
+    name = get_name
+    case String.downcase(get_cow_lover) do
+      "y" ->
+        IO.puts "Great! Here's a cow for you #{name}"
+        IO.puts cow_art
+      "n" ->
+        IO.puts "That's a shame, #{name}"
+      _ ->
+        IO.puts "You should have entered 'y' or 'n'."
+    end
+  end
+
+  @doc """
+  prints the cow art
+  """
+  def cow_art do
+    path = Path.expand("../support/cow.txt", __DIR__)
+    case File.read(path) do
+      {:ok, art} -> art
+      {:error, _} -> IO.puts "Error: cow.txt file not found"; System.halt(1)
+    end
+  end
+end
+
+ExUnit.start
+
+
+defmodule InputOutputTest do
+  use ExUnit.Case
+  import String
+
+  test "checks if cow_art returns string from support/cow.txt" do
+    art = CowInterrogator.cow_art
+    assert trim(art) |> first == "("
+  end
+end
+
+CowInterrogator.interrogate
